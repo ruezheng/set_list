c87246060c10af57d5d73de6ba2e9e1781316f2d
Brian Zanti <brianzanti@gmail.com>
2020-12-08 12:23:23 -0700

    adds artist update

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get '/artists/new', to: 'artists#new'
   post '/artists', to: 'artists#create'
   delete '/artists/:id', to: 'artists#destroy'
+  get '/artists/:id/edit', to: 'artists#edit'
+  patch "/artists/:id", to: 'artists#update'
+
 end
