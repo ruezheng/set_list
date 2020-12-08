94e9ccf32654bc4f3b74626b338cd8c77a9a216d
Brian Zanti <brianzanti@gmail.com>
2020-12-08 09:02:00 -0700

    wip passing data

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,17 @@
+class ArtistsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    Artist.create(artist_params)
+    redirect_to '/artists'
+  end
+
+  private
+  def artist_params
+    params.permit(:name)
+  end
+end
