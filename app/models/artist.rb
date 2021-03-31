4feb3b2ab7463aac7b20d2b24e91a88ab5c6749d
Brian Zanti <brianzanti@gmail.com>
2021-03-31 09:18:49 -0600

    adds prys

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,10 +2,12 @@ class Artist < ApplicationRecord
   has_many :songs
 
   def average_song_length
+    binding.pry
     songs.average(:length)
   end
 
   def self.by_name
+    binding.pry
     order(:name)
   end
 end
